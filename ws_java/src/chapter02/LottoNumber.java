4304791d9bc07b8f72cbca26cfd13ecf8c6ed275
yunachooi <glasodyyn@gmail.com>
2025-03-13 17:45:58 +0900

    2025-03-13

---
+++ ws_java/src/chapter02/LottoNumber.java
@@ -0,0 +1,27 @@
+package chapter02;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LottoNumber {
+
+	public static void main(String[] args) {
+		
+		// 중복되지 않는 로또 번호 추출기
+		int[] lotto = new int[6];
+		Random rd = new Random();
+		
+		for(int i = 0; i < 6; i++) {
+			int rdNum = rd.nextInt(45) + 1;
+			lotto[i] = rdNum;
+			
+			for(int j = 0; j < i; j++) {
+				if (lotto[i] == lotto[j]) {
+					lotto[i] = rd.nextInt(45) + 1;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(lotto));
+	}
+
+}
