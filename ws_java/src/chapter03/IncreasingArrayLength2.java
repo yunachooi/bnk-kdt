8fc99c21ce37a3c9f10834359580f95adc32ab0b
yunachooi <glasodyyn@gmail.com>
2025-03-20 17:49:29 +0900

    2025-03-20

---
+++ ws_java/src/chapter03/IncreasingArrayLength2.java
@@ -0,0 +1,46 @@
+package chapter03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IncreasingArrayLength2 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static int[] increasing(int[] A) {
+		int[] B = new int[A.length + 5];
+
+		B = new int[A.length + 5];
+
+		for (int i = 0; i < A.length; i++) {
+			B[i] = A[i];
+		}
+
+		A = B;
+		return A;
+	}
+
+	public static void main(String[] args) {
+		int[] A = new int[5];
+		int cnt = 1;
+
+		while(true) { // A 배열 생성
+			System.out.print(cnt + ") 숫자를 입력하세요 >> ");
+			A[cnt - 1] = sc.nextInt();
+			
+			if(A[cnt - 1] == -1) {
+				A[cnt - 1] = 0;
+				System.out.println("-------------------");
+				System.out.println(Arrays.toString(A));
+				break;
+			}
+			
+			cnt += 1;
+			
+			if(A.length == cnt - 1) {
+				A = increasing(A);
+			}
+		}
+
+	}
+
+}
