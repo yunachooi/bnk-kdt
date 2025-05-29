cd0c7cc230540e81d74b70437fb0f9e9e6bc9965
yunachooi <glasodyyn@gmail.com>
2025-05-29 12:22:43 +0900

    2025-05-29

---
+++ jpaEx01/src/main/java/com/example/jpaEx01/JpaEx01Application.java
@@ -0,0 +1,13 @@
+package com.example.jpaEx01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEx01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEx01Application.class, args);
+	}
+
+}
