40a00060113fcbca2c378515a127d64ac9cbb631
yunachooi <glasodyyn@gmail.com>
2025-06-02 17:42:29 +0900

    2025-06-02

---
+++ ex01.dart
@@ -0,0 +1,26 @@
+void main() {
+  final String name = '블랙핑크';
+
+  final DateTime now = DateTime.now();
+  print(now);
+
+  String s = 'abcd';
+  print(s);
+  int n = 200;
+  print(n);
+  double d = 3.14;
+  print(d);
+  bool tf = true;
+  print(tf);
+
+  List<String> blackPinkList = ['리사', '지수', '제'];
+  blackPinkList[2] = '제니';
+  blackPinkList.add('로제');
+  print(blackPinkList);
+
+  //where : List에 있는 값들을 순서대로 순회하면서 특정 조건에 맞는 값만 필터링하는 데 사용.
+  //매개변수의 함수를 입력해야하며, 입력된 함수는 기존 값을 하나의 매개 변수로 입력받는다.
+  //각 값 별로 true를 반환하면 값을 유지하고, false로 반환하면 값을 버린다.
+  final newList = blackPinkList.where((name) => name == '리사' || name == '지수');
+  print(newList.toList());
+}
